c078a5b7716bf7b722dd503d88aa7310a26978b6
Letícia Castro <aicitelks@gmail.com>
2021-05-28 22:30:56 -0300

    Desafio 10 concluido (:heavy_check_mark:)

--- src/challenges2.js
+++ src/challenges2.js
@@ -3,25 +3,42 @@ function techList(arrayDeNomesTecnologias, name) {
   let objeto = {};
   let lista = [];
 
-  // Criar um objeto para cada tecnologia
-  for (i = 0; i < arrayDeNomesTecnologias.length; i += 1) {    
-    objeto = {
-      tech: arrayDeNomesTecnologias[i],
-      name: name
+  if (arrayDeNomesTecnologias.length != 0){
+    // Criar um objeto para cada tecnologia
+    for (i = 0; i < arrayDeNomesTecnologias.length; i += 1) {
+      objeto = {
+        tech: arrayDeNomesTecnologias[i],
+        name: name
+      }
+      // inserir os objetos em uma nova lista
+      lista.push(objeto);
     }
-    // inserir os objetos em uma nova lista
-    lista.push(objeto);
+    /** 
+    * Ordenar em ordem crescente a partir do campo tech do objeto
+    * Source: https://pt.stackoverflow.com/a/46601
+    * A função de ordenação, sort(), recebe uma função como parâmetro e esta por sua vez faz as comparações
+    * para realizar a ordenação. 
+    * O cálculo da ordenação é feito internamente e o retorno é da lista já ordenada
+    */
+    lista.sort(function (a, b) {
+      if (a.tech < b.tech)
+        return -1; // retorna negativo se for menor
+      if (a.tech > b.tech)
+        return 1; // retorna positivo se for maior
+      return 0; // retorna 0 se as strigns forem iguais ou equivalentes
+    });
+    // Retornar a lista de objetos ordenada pelo campo tech
+    return lista;
+    
+  } else {
+    return 'Vazio!'
   }
-  // ordenar em ordem crescente a partir do campo tech do objeto
-  
-  // retornar a lista de obejtos ordana pelo campo tech
-  return lista;
 
 }
 
-let nomesTecnologias = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
-let nomeProprio = 'Letícia Castro';
-console.log(techList(nomesTecnologias, nomeProprio));
+// let nomesTecnologias = ["React", "Jest", "HTML", "CSS", "JavaScript"];
+// let nomeProprio = 'Letícia Castro';
+// console.log(techList(nomesTecnologias, nomeProprio));
 
 // Desafio 11
 function generatePhoneNumber() {
